1fdb504270073d9764a4134e5d08dec21e9b35fd
oliverwinburn <owinburn15@outlook.com>
2023-07-24 22:57:50 +1000

    actually output to a file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
-int main()
-{
-    std::string data;
-    std::cout << "Give string: ";
-    std::getline(std::cin, data);
-    std::cout << "Input: " << data;
+int main() {
+    constexpr auto dimx = 16u;
+    constexpr auto dimy = 16u;
+    std::string WHITE{static_cast<char>(255), static_cast<char>(255), static_cast<char>(255)};
+    std::string BLACK{static_cast<char>(0), static_cast<char>(0), static_cast<char>(0)};
+
+    std::ofstream file("out.ppm", std::ios_base::out | std::ios_base::binary);
+    file << "";
+    file << "P6\n"
+         << dimx << " " << dimy << " "
+         << "\n255\n";
+
+    std::string output = "";
+
+    for (auto j = 0u; j < dimy; ++j) {
+        for (auto i = 0u; i < dimx; ++i) {
+            output += (i < 7 && j < 10) ? WHITE : BLACK;
+        }
+    }
+    file << output;
+    file.close();
+
     return 0;
 }
