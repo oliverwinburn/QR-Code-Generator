fa76fdddbec499c62fc661aed91352d9d163f28d
oliverwinburn <owinburn15@outlook.com>
2023-07-25 18:25:09 +1000

    move image rendering to a function

--- main.cpp
+++ main.cpp
@@ -1,28 +1,39 @@
 #include <fstream>
-#include <iostream>
+#include <list>
 #include <string>
 
-int main() {
-    constexpr auto dimx = 16u;
-    constexpr auto dimy = 16u;
+void renderImage(std::list<bool> const &pixels, int dimx, int dimy) {
     std::string WHITE{static_cast<char>(255), static_cast<char>(255), static_cast<char>(255)};
     std::string BLACK{static_cast<char>(0), static_cast<char>(0), static_cast<char>(0)};
+    std::string output = "";
+
+    for (auto const &i : pixels) {
+        output += (i) ? WHITE : BLACK;
+    }
 
     std::ofstream file("out.ppm", std::ios_base::out | std::ios_base::binary);
-    file << "";
     file << "P6\n"
          << dimx << " " << dimy << " "
          << "\n255\n";
 
-    std::string output = "";
+    file << output;
+    file.close();
+}
+
+int main() {
+    constexpr auto dimx = 16;
+    constexpr auto dimy = 16;
+
+    std::list<bool> pixels{};
 
     for (auto j = 0u; j < dimy; ++j) {
         for (auto i = 0u; i < dimx; ++i) {
-            output += (i < 7 && j < 10) ? WHITE : BLACK;
+            bool pixel = (i % 2 && j % 2) ? 1 : 0;
+            pixels.push_back(pixel);
         }
     }
-    file << output;
-    file.close();
+
+    renderImage(pixels, dimx, dimy);
 
     return 0;
 }
